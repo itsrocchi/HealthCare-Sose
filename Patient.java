a92cc05a98a21bc088980072dff16fb9ba078f1e
itsrocchi <l.rocchi13@gmail.com>
2024-07-10 11:55:50 +0200

    test commit lolli

--- Patient.java
+++ Patient.java
@@ -9,5 +9,11 @@ public class Patient {
     private String address;
     private String email;
 
+    public Date getBirthDate() {
+        return birthDate;
+    }
 
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
 }
