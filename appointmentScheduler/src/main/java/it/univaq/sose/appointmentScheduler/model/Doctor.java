00df5121b3d58b39659af03667e3f53ec7bca197
BeatriceTomassi <beatrice.tomassi@student.univaq.it>
2024-07-10 17:38:58 +0200

    git day 1 bea - rev parte2

---
+++ appointmentScheduler/src/main/java/it/univaq/sose/appointmentScheduler/model/Doctor.java
@@ -0,0 +1,53 @@
+package it.univaq.sose.appointmentScheduler.model;
+
+import java.util.List;
+
+public class Doctor {
+
+    private Long id;
+    private String name;
+    private String surname;
+    private String specialization;
+    private List<String> appointments;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public List<String> getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(List<String> appointments) {
+        this.appointments = appointments;
+    }
+}
