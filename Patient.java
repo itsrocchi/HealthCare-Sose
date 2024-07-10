9327c579c7e489673257f08ebb52b37fecb6fc84
BeatriceTomassi <beatrice.tomassi@student.univaq.it>
2024-07-10 11:52:00 +0200

    test commit

---
+++ Patient.java
@@ -0,0 +1,13 @@
+import java.sql.Date;
+
+public class Patient {
+
+    private String name;
+    private String surname;
+    private String CF;
+    private Date birthDate;
+    private String address;
+    private String email;
+
+
+}
